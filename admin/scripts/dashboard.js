d2023ab06a84e689424f7e28d6ff9cbcd65b3616
echava12345 <miguelechavarria1002@outlook.es>
2025-06-16 19:58:40 -0500

    Update dashboard.js

--- admin/scripts/dashboard.js
+++ admin/scripts/dashboard.js
@@ -15,17 +15,27 @@ let products = JSON.parse(localStorage.getItem("products")) || [];
 
 addProductForm.onsubmit = function(e) {
     e.preventDefault();
-    const name = document.getElementById("productName").value;
+const name = document.getElementById("productName").value;
     const detail = document.getElementById("productDetail").value;
+    const price = parseInt(document.getElementById("productPrice").value);
+    const categoria = document.getElementById("productCategory").value;
     const imgInput = document.getElementById("productImage");
     const reader = new FileReader();
 
+
     reader.onload = function(event) {
-        products.push({ name, detail, image: event.target.result });
+        products.push({
+            name,
+            detail,
+            precio: price,
+            categoria,
+            image: event.target.result
+        });
         localStorage.setItem("products", JSON.stringify(products));
         renderProducts();
         addProductForm.reset();
     };
+
 
     if (imgInput.files[0]) reader.readAsDataURL(imgInput.files[0]);
 };
@@ -36,9 +46,11 @@ function renderProducts() {
         return;
     }
     productList.innerHTML = products.map((p, i) =>
-        `<div>
+       `<div>
             <img src="${p.image}" alt="Producto" width="100"><br>
             <strong>${p.name}</strong><br>
+            <span>Categoría: ${p.categoria}</span><br>
+            <span>Precio: $${p.precio.toLocaleString('es-CO')}</span><br>
             ${p.detail}<br>
             <button onclick="deleteProduct(${i})" style="background:var(--color-corazon);color:white;padding:0.2em 1em;border-radius:10px;border:none;cursor:pointer;margin-top:8px;">Eliminar</button>
         </div>`
